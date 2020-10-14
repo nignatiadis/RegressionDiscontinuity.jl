730d3808e891c98e302ec8e010eeaaeed763334c
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2020-10-14 12:28:44 -0700

    actually point to feather file

--- src/load_example_data.jl
+++ src/load_example_data.jl
@@ -1,6 +1,6 @@
 function load_rdd_data(dataset::Symbol)
     if dataset == :lee08
-        lee08_path = joinpath(dirname(@__FILE__), "..", "data", "lee08.rda")
+        lee08_path = joinpath(dirname(@__FILE__), "..", "data", "lee08.feather")
 
         lee08 = Feather.read(lee08_path)
         ZsR = RunningVariable(lee08.margin ./ 100; cutoff = 0.0, treated = :≥)
