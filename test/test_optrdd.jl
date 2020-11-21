211b460d023f7b0a0d93bc4eca8ef65d78be3ae9
Evan Munro <evan.munro@gmail.com>
2020-11-20 17:47:28 -0800

    remove test

--- test/test_optrdd.jl
+++
@@ -1,7 +0,0 @@
-using MosekTools, CSV, DataFrames
-data = DataFrame(CSV.File("/Users/evanmunro/Documents/Github/rdd-gan/data/cleaned/m_math.csv"))
-ZsR = RunningVariable(data.x[abs.(data.x).<10^6]; cutoff = 0.0, treated = :≥)
-Ys = data.y[abs.(data.x).<10^6]
-data = RDData(Ys, ZsR)
-
-result = fit(MinMaxOptRD(B=0.01, solver=Mosek.Optimizer), data)
